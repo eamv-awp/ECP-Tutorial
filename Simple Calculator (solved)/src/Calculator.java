77e98c53f38141d78422a83b22b3e40ee0114753
Anders Petersen <awp@eamv.dk>
2015-02-20 09:21:25 +0100

    Functionality moved from main method to CalculatorController object.

--- Simple Calculator (solved)/src/Calculator.java
+++ Simple Calculator (solved)/src/Calculator.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import logic.CalculatorController;
 
 /**
  * Source: http://codereview.stackexchange.com/questions/40615/simple-java-calculator
@@ -8,40 +8,6 @@ public class Calculator {
      * @param args
      */
     public static void main(String[] args) {
-        double n1, n2;
-        String operation;
-        Scanner scannerObject = new Scanner(System.in);
-
-        System.out.println("Enter first number");
-        n1 = scannerObject. nextDouble();
-
-        System.out.println("Enter second number");
-        n2 = scannerObject. nextDouble();
-
-        Scanner op = new Scanner(System.in);
-        System.out.println("Enter your operation");
-        operation = op.next();
-
-        switch (operation)  {
-        case "+":
-            System.out.println("Your answer is " + (n1 + n2));
-            break;
-
-        case "-":
-            System.out.println("Your answer is " + (n1 - n2));
-            break;
-
-        case "/":
-            System.out.println("Your answer is " + (n1 / n2));
-            break;
-
-        case "*":
-            System.out.println("Your asnwer is " + (n1 * n2));
-            break;
-
-        default:
-            System.out.println("Je ne sais pas");
-
-        }
+    	new CalculatorController();
     }
 }
